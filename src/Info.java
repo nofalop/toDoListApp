d09c953dcbdf6fc1f610513e106baa135bb90c8b
Nofal <nofalreal62@gmail.com>
2025-04-26 20:12:43 +0400

    Start

---
+++ src/Info.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+import java.util.Dictionary;
+import java.util.Hashtable;
+import java.util.List;
+
+public class Info {
+	public static List<String> Task= new ArrayList<>();
+	public static Dictionary<String,Boolean> taskInfo= new Hashtable<>();
+}
