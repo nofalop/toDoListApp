010adc945c60fdc76120ee674c41f0f0de210a40
Nofal <nofalreal62@gmail.com>
2025-04-27 20:41:38 +0400

    Clear Func + fixed bugs

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 public class Main {
 	public static void main(String[] args){
+
 		while (true){
 			System.out.println(
 					"""
@@ -11,22 +12,40 @@ public class Main {
 					4-Quit"""
 			);
 			UserInput();
+			clearConsole();
 		}
 	}
 
-	private static void UserInput(){
+	private static void UserInput() {
 		Scanner scanner = new Scanner(System.in);
 		String Choice = scanner.nextLine().trim().toLowerCase();
-		if(Choice.isEmpty()){
+		if (Choice.isEmpty()) {
 			System.out.println("Choice can't be empty ");
 			return;
 		}
-		switch (Choice){
+		switch (Choice) {
 			case "add task" -> System.out.println("add task");
 			case "mark task" -> System.out.println("mark Task");
 			case "remove task" -> System.out.println("remove task");
 			case "quit" -> System.exit(1);
-			default -> System.out.println("Enter a valid choice");
+			default -> {
+				clearConsole();
+				System.out.println("Enter a valid choice");
+			}
+		}
+	}
+
+	private static void clearConsole(){
+		try {
+			//checks if the os is windows
+			if (System.getProperty("os.name").contains("Windows")) {
+				new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+			} else {
+				System.out.print("\033[H\033[2J");
+				System.out.flush();
+			}
+		} catch (Exception e) {
+			System.out.println("Could not clear console.");
 		}
 	}
 }
