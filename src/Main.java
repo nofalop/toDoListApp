e2e761e839b3cd8139591e4714dca9bb4c51854f
Nofal <nofalreal62@gmail.com>
2025-04-27 19:36:13 +0400

    UserInput and error handling

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,32 @@
+import java.util.Scanner;
 public class Main {
 	public static void main(String[] args){
-
+		while (true){
+			System.out.println(
+					"""
+					TodoList
+					1-Add Task
+					2-mark Task
+					3-Remove Task
+					4-Quit"""
+			);
+			UserInput();
+		}
 	}
 
+	private static void UserInput(){
+		Scanner scanner = new Scanner(System.in);
+		String Choice = scanner.nextLine().trim().toLowerCase();
+		if(Choice.isEmpty()){
+			System.out.println("Choice can't be empty ");
+			return;
+		}
+		switch (Choice){
+			case "add task" -> System.out.println("add task");
+			case "mark task" -> System.out.println("mark Task");
+			case "remove task" -> System.out.println("remove task");
+			case "quit" -> System.exit(1);
+			default -> System.out.println("Enter a valid choice");
+		}
+	}
 }
